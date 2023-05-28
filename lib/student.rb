3c3be5b3900d8a9374d7020f7a519e891e425b02
brigitteorina <brigitteorina@gmail.com>
2023-05-28 11:10:56 +0300

    complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+class Student < User
+  def initialize(first_name, last_name)
+    super(first_name, last_name)
+    @knowledge = []
+  end
 
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
+
+steve = Student.new("Steve", "Johnson")
+avi = Teacher.new("Avi", "Cohen")
+
+knowledge = avi.teach
+steve.learn(knowledge)
